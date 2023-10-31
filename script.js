b08df394ef56122c64df7d057fc94a14acd9b042
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-31 08:23:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function aveSpd(upTime, upSpd, downSpd) {
-  // your code here
+  const totalDistance = upSpd * (upTime / 60); 
+  const totalTime = upTime + (totalDistance / downSpd) * 60;
+  const averageSpeed = (2 * totalDistance) / (totalTime / 60);
+
+  return averageSpeed;
 }
+console.log(aveSpd(18, 20, 60));
+console.log(aveSpd(30, 10, 30));
+console.log(aveSpd(30, 8, 24)); 
+
 
 // Do not change the code below
 const upTime = prompt("Enter upTime: ");
 const downTime = prompt("Enter downTime: ");
 const downSpd = prompt("Enter downSpd: ");
 
-alert(aveSpd(upTime, downTime, downSpd));
+//alert(aveSpd(upTime, downTime, downSpd));
